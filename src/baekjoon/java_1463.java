af9c58f251ab78841d862bb69cb112a15fa5acb0
eeminsu <alstn9623@naver.com>
2021-12-03 20:52:46 +0900

    1463

--- src/baekjoon/java_1463.java
+++ src/baekjoon/java_1463.java
@@ -1,32 +1,42 @@
 package baekjoon;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class java_1463 {
 
+	public static int[] dp = new int[1000001];
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		Arrays.fill(dp, -1);
 		int X = Integer.parseInt(br.readLine());
-		int cnt = 0;
+		dp[0] = 0;
+		dp[1] = 0;
 		
-		while(X != 1) {
-			if(X%3 == 0) {
-				X /= 3;
-				cnt++;
+		System.out.println(rec(X));
+		
+	}
+	
+	public static int rec(int N) {
+		
+		if(dp[N] == -1) {
+			if(N%6 == 0) {
+				dp[N] = Math.min(rec(N-1), Math.min(rec(N/3), rec(N/2)))+1;
 			}
-			else if(X%2 == 0) {
-				X /= 2;
-				cnt++;
+			else if(N%3 == 0) {
+				dp[N] = Math.min(rec(N/3), rec(N-1))+1;
 			}
-			else if(X%3 != 0 && X%2 != 0) {
-				X -= 1;
-				cnt++;
+			else if(N%2 == 0) {
+				dp[N] = Math.min(rec(N/2), rec(N-1))+1;
+			}
+			else {
+				dp[N] = rec(N-1)+1;
 			}
 		}
 		
-		System.out.println(cnt);
-		
+		return dp[N];
 	}
 
 }
